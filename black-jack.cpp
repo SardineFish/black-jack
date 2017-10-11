932cdc3ab47bd3afe355099a4b8c1df41660dc3b
SardineFish <Sardine@live.cn>
2017-10-11 21:55:04 +0800

    test

--- black-jack.cpp
+++ black-jack.cpp
@@ -1,5 +1,15 @@
 #include<stdio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+void init();
 int main()
 {
+	string name="我是谁";
+	printf("%s\n",name);
 	return 0;
+}
+void init()
+{
+	
 }
